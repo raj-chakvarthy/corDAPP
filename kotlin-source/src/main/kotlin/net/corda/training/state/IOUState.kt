0c2e3fe86f3485a5859ff6b98914fa61d3856fd1
raj-chakvarthy <rchakv@gmail.com>
2018-10-11 14:44:50 -0400

    Initial Commit

---
+++ kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
@@ -0,0 +1,19 @@
+package net.corda.training.state
+
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.ContractState
+import net.corda.core.identity.Party
+import java.util.*
+
+/**
+ * This is where you'll add the definition of your state object. Look at the unit tests in [IOUStateTests] for
+ * instructions on how to complete the [IOUState] class.
+ *
+ * Remove the "val data: String = "data" property before starting the [IOUState] tasks.
+ */
+data class IOUState(val amount: Amount<Currency>,
+                    val lender: Party,
+                    val borrower: Party,
+                    val paid: Amount<Currency>): ContractState {
+    override val participants: List<Party> get() = listOf()
+}
